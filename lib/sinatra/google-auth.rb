7b27de05d3f72ff568e271dd96e1d5b87b393f77
Tres Trantham <tres@trestrantham.com>
2012-07-16 23:12:43 -0400

    Write user_info to session

--- lib/sinatra/google-auth.rb
+++ lib/sinatra/google-auth.rb
@@ -33,8 +33,8 @@ module Sinatra
       def handle_authentication_callback
         unless session["user"]
           user_info = request.env["omniauth.auth"].info
-          on_user(user_info) if respond_to? :on_user
           session["user"] = Array(user_info.email).first.downcase
+          session["user_info"] = user_info
         end
 
         url = session['google-auth-redirect'] || to("/")
